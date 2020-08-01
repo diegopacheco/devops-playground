c5bed2e2daf5b5260a24a3f8970320809f1103a4
diegopacheco <diego.pacheco.it@gmail.com>
2020-07-31 23:23:51 -0300

    terraform functions fun

--- terraform-functions/main.tf
+++ terraform-functions/main.tf
@@ -1,11 +1,19 @@
 locals{
   json_data = jsondecode(file("${path.module}/data.json"))
   rs_capital = upper(local.json_data.RS)
+  rj_capital = lookup(local.json_data, "RJ", "Rio de Janeiro")
+  encoded_poa = base64encode(local.json_data.RS)
 }
 
 output "json" {
   value = "${local.json_data}"
 }
 output "rs-capital" {
-  value = "${local.rs_capital}"
+  value = "Capital: ${local.rs_capital} Encoded: ${local.encoded_poa}"
+}
+output "rj-capital" {
+  value = "${local.rj_capital}"
+}
+output "all-capitals" {
+  value = "${values(local.json_data)}"
 }
