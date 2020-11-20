c3a6e52b2415bb704109509b0f6d0aacaddca70f
diegopacheco <diego.pacheco.it@gmail.com>
2020-11-20 11:51:48 -0300

    k8ssandra

---
+++ k8ssandra/install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+minikube start --memory=8192 --cpus=8 -p k8ssandra 
+helm repo add k8ssandra https://helm.k8ssandra.io/
+helm repo add traefik https://helm.traefik.io/traefik
+helm repo update
+helm install k8ssandra-tools k8ssandra/k8ssandra
+helm install k8ssandra-cluster-a k8ssandra/k8ssandra-cluster
