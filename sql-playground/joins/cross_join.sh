6423cefb3210ffa4043a11a57f44cf13ed2be750
diegopacheco <diego.pacheco.it@gmail.com>
2024-01-17 23:45:13 -0800

    sql playground

---
+++ sql-playground/joins/cross_join.sh
@@ -0,0 +1,15 @@
+#/bin/bash
+
+tiv ../images/cross-join.png
+
+echo "
+USE sql_playground;
+
+SELECT 
+    u.*,
+    p.*
+FROM 
+    users u
+CROSS JOIN userprofile p
+ON u.auid = p.auid;" | \
+mysql -uroot -ppass -h127.0.0.1 -P3325
