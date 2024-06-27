d5a5702785e59f44869737eba69ac3feeba5a80d
diegopacheco <diego.pacheco.it@gmail.com>
2024-06-27 00:55:50 -0700

    mysql partition

--- mysql-partition/README.md
+++ mysql-partition/README.md
@@ -2,3 +2,18 @@
 ```
 ./run-mysql-container.sh
 ```
+
+### Errors
+
+```
+mysql> CREATE TABLE IF NOT EXISTS person (
+    ->           id INT AUTO_INCREMENT PRIMARY KEY,
+    ->           first_name VARCHAR(255) NOT NULL,
+    ->           last_name VARCHAR(255) NOT NULL,
+    ->           created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    ->           created_year int,
+    ->           UNIQUE KEY (id,created_year)
+    ->       ) PARTITION BY HASH(created_year);
+ERROR 1503 (HY000): A PRIMARY KEY must include all columns in the table's partitioning function
+```
+
--- mysql-partition/run-mysql-container.sh
+++ mysql-partition/run-mysql-container.sh
@@ -9,13 +9,15 @@ echo "Creating person db... "
 echo "CREATE DATABASE person;" | mysql -uroot -ppass -h127.0.0.1 -P3325
 
 sleep 3
-echo "Creating person table... "
+echo "Creating person table with paritioning... "
 echo "use person; CREATE TABLE IF NOT EXISTS person (
           id INT AUTO_INCREMENT PRIMARY KEY,
           first_name VARCHAR(255) NOT NULL,
           last_name VARCHAR(255) NOT NULL,
-          created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-      ) ENGINE=INNODB;;" | mysql -uroot -ppass -h127.0.0.1 -P3325
+          created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+          created_year int,
+          UNIQUE KEY (id,created_year)
+      ) PARTITION BY HASH(created_year);" | mysql -uroot -ppass -h127.0.0.1 -P3325
 
 echo "use person; select * from  person" | mysql -uroot -ppass -h127.0.0.1 -P3325
 echo "DONE."
