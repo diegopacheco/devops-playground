854edc91fd7ecf0b22d6a7f8eb06fab9b58edc71
diegopacheco <diego.pacheco.it@gmail.com>
2024-07-24 02:17:12 -0700

    liquibase docker

---
+++ liquibase-mysql-docker/scripts/insert-products-data.sql
@@ -0,0 +1,6 @@
+INSERT INTO products (name, price, quantity) VALUES ('Apple', 0.99, 100);
+INSERT INTO products (name, price, quantity) VALUES ('Banana', 0.59, 200);
+INSERT INTO products (name, price, quantity) VALUES ('Cherry', 2.99, 50);
+INSERT INTO products (name, price, quantity) VALUES ('Elderberry', 3.99, 25);
+INSERT INTO products (name, price, quantity) VALUES ('Fig', 1.99, 75);
+INSERT INTO products (name, price, quantity) VALUES ('Grape', 1.49, 150);
