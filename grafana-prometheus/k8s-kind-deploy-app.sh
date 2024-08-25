db9d3004122b254d9122d5c55ce20476d7ccbc60
diegopacheco <diego.pacheco.it@gmail.com>
2024-08-25 01:18:51 -0700

    k8s / kind / grafana-prometheus poc

--- grafana-prometheus/k8s-kind-deploy-app.sh
+++ grafana-prometheus/k8s-kind-deploy-app.sh
@@ -9,6 +9,10 @@ kubectl apply -f specs/sb3-netty-service.yaml --force
 kubectl apply -f specs/grafana-deployment.yaml --force
 kubectl apply -f specs/grafana-service.yaml --force
 
+kubectl apply -f specs/node-exporter-daemonset.yaml --force
+kubectl apply -f specs/cadvistor-daemonset.yaml --force
+
 kubectl apply -f specs/prometheus-deployment.yaml --force
 kubectl apply -f specs/prometheus-service.yaml --force
 kubectl apply -f specs/prometheus-sb-configmap.yaml --force
+
