0c84e5a0b777fa4f421daaf4296bccb425801f6f
diegopacheco <diego.pacheco.it@gmail.com>
2024-12-20 23:07:37 -0800

    Marquez

---
+++ Marquez-lineage-fun/docker/dags/dag1.py
@@ -0,0 +1,48 @@
+from __future__ import annotations
+import time
+import random
+
+import pendulum
+from airflow.decorators import dag, task
+from airflow.operators.empty import EmptyOperator
+from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
+from airflow.datasets import Dataset
+
+SQL="""CREATE TABLE IF NOT EXISTS airflowsample (
+    col1 VARCHAR(255), 
+    col2 VARCHAR(255)
+)"""
+
+@dag(
+    schedule='@hourly',
+    start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
+    catchup=False,
+    tags=["example"],
+    dag_display_name="Flaky DAG",
+)
+
+def example_display_name_brkn():
+
+    sample_task_1 = EmptyOperator(
+        task_id="sample_task_1",
+        task_display_name="Sample Task 1",
+    )
+
+    sample_task_2 = SQLExecuteQueryOperator(
+        task_id="sample_task_3",
+        sql=SQL,
+        conn_id="POSTGRES_DEFAULT",
+    )
+
+    @task(
+        task_display_name="Sample Task 3",
+        outlets=[Dataset("sample_pg_table")]
+    )
+    def sample_task_3():
+        pers = [0, 60, 120, 'fail']
+        per = random.choice(pers)
+        time.sleep(per)
+
+    sample_task_1 >> sample_task_2 >> sample_task_3()
+
+example_display_name_brkn()
---
+++ Marquez-lineage-fun/docker/dags/dag2.py
@@ -0,0 +1,44 @@
+from __future__ import annotations
+import time
+import random
+
+import pendulum
+from airflow.decorators import dag, task
+from airflow.operators.empty import EmptyOperator
+from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
+from airflow.datasets import Dataset
+
+SQL_1="INSERT INTO airflowsample (col1) VALUES ('row')"
+
+SQL_2="DROP TABLE airflowsample"
+
+@dag(
+    schedule=[Dataset("sample_pg_table")],
+    start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
+    catchup=False,
+    tags=["example"],
+    dag_display_name="Flaky DAG - 2",
+)
+
+def example_insert_brkn():
+
+    sample_task_1 = EmptyOperator(
+        task_id="sample_insert_task_1",
+        task_display_name="Sample Task 1",
+    )
+
+    sample_task_2 = SQLExecuteQueryOperator(
+        task_id="sample_insert_task_2",
+        sql=SQL_1,
+        conn_id="POSTGRES_DEFAULT",
+    )
+
+    sample_task_3 = SQLExecuteQueryOperator(
+        task_id="sample_insert_task_3",
+        sql=SQL_2,
+        conn_id="POSTGRES_DEFAULT",
+    )
+
+    sample_task_1 >> sample_task_2 >> sample_task_3
+
+example_insert_brkn()
