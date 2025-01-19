1cb03a44fcd0406d5a31f8aa74ffcb5d115bb752
diegopacheco <diego.pacheco.it@gmail.com>
2025-01-18 23:08:09 -0800

    postgres-anonymizer poc

--- postgresql-anonymizer/create-db.sh
+++ postgresql-anonymizer/create-db.sh
@@ -6,27 +6,28 @@ docker exec -i anon_quickstart psql -U postgres -c "CREATE DATABASE demo;"
 # Connect to the demo database and set it up
 docker exec -i anon_quickstart psql -U postgres <<EOF
 \connect demo
+
 CREATE TABLE IF NOT EXISTS people AS
     SELECT  153478       AS id,
             'Sarah'      AS firstname,
             'Conor'      AS lastname,
             '0609110911' AS phone
 ;
-BEGIN;
+
 INSERT INTO people (id, firstname, lastname, phone) VALUES
     (153478, 'Sarah', 'Conor', '0609110911'::text);
-COMMIT;
-CREATE EXTENSION IF NOT EXISTS anon;
+
+CREATE EXTENSION anon;
 ALTER DATABASE demo SET anon.transparent_dynamic_masking TO true;
-DO \$\$
-BEGIN
-   IF NOT EXISTS (SELECT FROM pg_roles WHERE rolname = 'skynet') THEN
-      CREATE ROLE skynet LOGIN;
-   END IF;
-END
-\$\$;
+
+CREATE ROLE skynet LOGIN;
 SECURITY LABEL FOR anon ON ROLE skynet IS 'MASKED';
-GRANT pg_read_all_data TO skynet;
-SECURITY LABEL FOR anon ON COLUMN people.lastname IS 'MASKED WITH FUNCTION anon.dummy_last_name()';
-SECURITY LABEL FOR anon ON COLUMN people.phone IS 'MASKED WITH FUNCTION anon.partial(phone,2,$$******$$,2)';
+GRANT pg_read_all_data to skynet;
+
+SECURITY LABEL FOR anon ON COLUMN people.lastname
+  IS 'MASKED WITH FUNCTION anon.dummy_last_name()';
+
+SECURITY LABEL FOR anon ON COLUMN people.phone
+  IS 'MASKED WITH FUNCTION anon.partial(phone,2,$$******$$,2)';
+
 EOF
