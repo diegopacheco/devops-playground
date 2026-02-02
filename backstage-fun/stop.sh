6e1e42cb6975f54022c6bbe69cf5f43b2efbd326
diegopacheco <diego.pacheco.it@gmail.com>
2026-02-01 21:56:45 -0800

    backstage

--- backstage-fun/stop.sh
+++ backstage-fun/stop.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+echo "Stopping Backstage container..."
+podman ps -q --filter "ancestor=roadiehq/community-backstage-image" | xargs -r podman stop 2>/dev/null
+podman ps -q --filter "publish=7000" | xargs -r podman stop 2>/dev/null
+
 echo "Stopping Kind cluster..."
 kind delete cluster --name backstage-cluster
 
